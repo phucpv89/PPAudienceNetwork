c1ecd4fb0efdd0a96b112fe959eca9cd333fbda4
phucpv <phucpv@vng.com.vn>
2017-10-16 15:38:56 +0700

    init

---
+++ Example/Pods/Target Support Files/PPAudienceNetwork/PPAudienceNetwork-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PPAudienceNetwork : NSObject
+@end
+@implementation PodsDummy_PPAudienceNetwork
+@end
---
+++ Example/Pods/Target Support Files/Pods-PPAudienceNetwork_Tests/Pods-PPAudienceNetwork_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PPAudienceNetwork_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_PPAudienceNetwork_Tests
+@end
